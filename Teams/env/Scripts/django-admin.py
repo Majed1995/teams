77c4c00eb1e00cf8a1e432d3fa31c46c61d128e0
Majed1995 <majed.alattar@zain.com>
2020-02-01 12:49:39 +0300

    finalproduct

---
+++ Teams/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\majed alattar\desktop\development\nui\teams\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Teams/team/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Team, Member
+
+class TeamForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+
+class MemberForm(forms.ModelForm):
+    class Meta:
+        model = Member
+        fields = '__all__'
